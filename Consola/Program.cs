a3615b0d0c401fcb486bab43b16377be36994bc5
nallley-lopez <lopezgarcializethnallelygmail.com>
2025-03-05 19:58:49 -0600

    Se agrego en la linea 21 el texto: "Sumar dos numeros"

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -18,7 +18,7 @@ namespace Consola
                 try
                 {
                     Console.WriteLine("Menu Principal Tec de Tlaxiaco1");
-                    Console.WriteLine("1. Sumar");
+                    Console.WriteLine("1. Sumar dos numeros");
                     Console.WriteLine("2. Restar");
                     Console.WriteLine("3. Multiplicar");
                     Console.WriteLine("4. Dividir");
